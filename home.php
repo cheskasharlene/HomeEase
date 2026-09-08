6e441f6e27253cb297761aaff637e51fe797c423
janamacandili <janamaemacandili@gmail.com>
2026-09-09 02:22:54 +0800

    fix + button

--- home.php
+++ home.php
@@ -42,7 +42,7 @@ if ($uid > 0) {
     href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&family=Poppins:wght@400;500;600;700;800&display=swap"
     rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/css/main.css?v=2.0" rel="stylesheet">
+  <link href="assets/css/main.css?v=<?= time() ?>" rel="stylesheet">
   <link href="assets/css/home.css?v=<?= time() ?>" rel="stylesheet">
 </head>
 
